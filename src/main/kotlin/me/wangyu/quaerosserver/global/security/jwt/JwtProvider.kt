b2dd0d2533152098f89955316e889a6660fc3e55
pea06 <101560095+pea06@users.noreply.github.com>
2023-10-20 08:56:34 +0900

    feat: jwt provider

---
+++ src/main/kotlin/me/wangyu/quaerosserver/global/security/jwt/JwtProvider.kt
@@ -0,0 +1,48 @@
+package me.wangyu.quaerosserver.global.security.jwt
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import me.wangyu.quaerosserver.domain.user.persistence.domain.User
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.security.Key
+import java.time.Duration
+import java.time.Instant
+import java.util.*
+import javax.annotation.PostConstruct
+
+@Component
+class JwtProvider {
+    @Value("\${quaeros.jwt.secretKey}")
+    private val secretKey: String? = null
+    private var key: Key? = null
+
+    @PostConstruct
+    fun init() {
+        this.key = Keys.hmacShaKeyFor(secretKey?.toByteArray());
+    }
+
+    fun generateToken(user: User): String {
+        val now: Instant = Instant.now()
+        return Jwts.builder()
+            .setSubject(user.email)
+            .setIssuedAt(Date.from(now))
+            .setExpiration(Date.from(now.plus(Duration.ofDays(3))))
+            .signWith(key)
+            .compact()
+    }
+
+    fun getAllClaimsFromToken(token: String): Claims {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).body
+    }
+
+    fun getEmailFormToken(token: String): String {
+        return getAllClaimsFromToken(token).subject
+    }
+
+    fun validateToken(token: String): Boolean {
+        val expiration: Date = getAllClaimsFromToken(token).expiration
+        return !expiration.before(Date())
+    }
+}
